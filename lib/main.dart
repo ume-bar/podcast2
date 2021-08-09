0997b6d0204037366090a15a3cc4dda0d9a36c04
ume_bar <kagari0915@gmail.com>
2021-08-09 16:22:57 +0900

    add with good looks

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'package:xml/xml.dart';
 import 'package:video_player/video_player.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -13,20 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: ''),
     );
   }
 }
@@ -34,23 +21,37 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   late VideoPlayerController _controller;
+  bool playing = false;
+  IconData playBtn = Icons.play_arrow;
+
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+
+  Widget slider(){
+    return Container(
+      width: 300.0,
+      child: Slider.adaptive(
+        activeColor: Colors.blue.shade800,
+        inactiveColor: Colors.grey.shade400,
+        value: position.inSeconds.toDouble(),
+        max: musicLength.inSeconds.toDouble(),
+        onChanged: (value){
+          seekToSec(value.toInt());
+        }),
+    );
+  }
+  void seekToSec(int sec){
+    Duration newPos = Duration(seconds: sec);
+    _controller.seekTo(newPos);
+  }
+
   @override
   void initState() {
     // アプリを起動時に一度だけ実行
@@ -71,6 +72,16 @@ class _MyHomePageState extends State<MyHomePage> {
         setState(() {});
       });
     });
+    // _controller.=(d){
+    //   setState(() {
+    //     musicLength = d;
+    //   });
+    // };
+    // _controller. =(p){
+    //   setState(() {
+    //     position = p;
+    //   });
+    // };
   }
 
   @override
@@ -82,46 +93,158 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Icon(Icons.music_video, size: 256),
-          ),
-          VideoProgressIndicator(
-            _controller,
-            allowScrubbing: true,
-          ),
-          _ProgressText(controller: _controller),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                onPressed: () {
-                  _controller
-                      .seekTo(Duration.zero)
-                      .then((_) => _controller.play());
-                },
-                icon: Icon(Icons.refresh),
-              ),
-              IconButton(
-                onPressed: () {
-                  _controller.play();
-                },
-                icon: Icon(Icons.play_arrow),
-              ),
-              IconButton(
-                onPressed: () {
-                  _controller.pause();
-                },
-                icon: Icon(Icons.pause),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+        body: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.green.shade800,
+                    Colors.green.shade200,
+                  ]),
+            ),
+            child: Padding(
+                padding: EdgeInsets.only(
+                  top: 48.0,
+                ),
+                child: Container(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 12.0),
+                        child: Text(
+                          "NHK WORLD RADIO JAPAN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 38.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 12.0),
+                        child: Text(
+                          "This is the latest news in English from NHK WORLD RADIO JAPAN.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      Center(
+                        child:Container(
+                          width: 280.0,
+                          height: 280.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            image: DecorationImage(
+                              image: AssetImage("assets/image.jpg"),
+                            )),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 18.0,
+                      ),
+                      Center(
+                        child: Text(
+                          "English News",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30.0),
+                            topRight: Radius.circular(30.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            // Container(
+                            //   width: 500.0,
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.center,
+                            //     crossAxisAlignment: CrossAxisAlignment.center,
+                            //     children: [
+                            //       Text(
+                            //         "${position.inMinutes}:${position.inSeconds.remainder(60)}",
+                            //       ),
+                            //       slider(),
+                            //       Text(
+                            //         "${musicLength.inMinutes}:${musicLength.inSeconds.remainder(60)}"
+                            //       )
+                            //     ],
+                            //   ),
+                            // ),
+                            VideoProgressIndicator(
+                              _controller,
+                              allowScrubbing: true,
+                            ),
+                            _ProgressText(controller: _controller),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  iconSize: 62.0,
+                                  color: Colors.blue,
+                                  onPressed: () {
+                                    if (!playing) {
+                                      _controller.play();
+                                      setState(() {
+                                        playBtn = Icons.pause;
+                                        playing = true;
+                                      });
+                                    } else {
+                                      _controller.pause();
+                                      setState(() {
+                                        playBtn = Icons.play_arrow;
+                                        playing = false;
+                                      });
+                                    }
+                                  },
+                                  icon: Icon(
+                                    playBtn,
+                                  ),
+                                ),
+                                IconButton(
+                                  iconSize: 62.0,
+                                  color: Colors.red,
+                                  onPressed: () {
+                                    _controller
+                                        .seekTo(Duration.zero)
+                                        .then((_) => _controller.pause());
+                                  },
+                                  icon: Icon(Icons.refresh),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))
+                    ]
+                  )
+                )
+              )
+            )
+          );
   }
 }
 
